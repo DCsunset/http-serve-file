fa15fbb026708d3d9a36c27cc28aa50413363561
DCsunset <DCsunset@users.noreply.github.com>
2019-09-05 06:00:04 -0700

    Add https support

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
 const http = require('http');
 const https = require('https');
 const Koa = require('koa');
-const path = require('path');
 const send = require('koa-send');
 const Router = require('koa-router');
+const fs = require('fs');
 
 const app = new Koa();
 const router = new Router();
 
 require('yargs')
-    .command('$0 <file(s)> [options]', 'Serve static file(s) via http or https.', () => {}, argv => {
-        for (const file of argv._) {
+    .command('$0 <file(s)> [options]', 'Serve static file(s) via http or https. (Double dots in file names are invalid)', () => {}, argv => {
+        const files = argv._.concat([argv['file(s)']]);
+        const protocol = argv.tls ? 'https' : 'http';
+        for (const file of files) {
             router.get(`/${file}`, async ctx => {
                 await send(ctx, file, { hidden: true });
             });
-            console.log(`Serving ${file}`);
-            console.log(`Serving at http://${argv.addr}:${argv.port}/${file}\n`);
         }
         app.use(router.routes());
         app.use(router.allowedMethods());
 
-        if (!argv.https) {
+        if (!argv.tls) {
             http.createServer(app.callback()).listen(argv.port, argv.addr);
+        }
+        else {
+            https.createServer({
+                key: fs.readFileSync(argv.key),
+                cert: fs.readFileSync(argv.cert)
+            }, app.callback()).listen(argv.port, argv.addr);
+        }
+
+        // Log
+        for (const file of files) {
+            console.log(`Serving ${file}`);
+            console.log(`Serving at ${protocol}://${argv.addr}:${argv.port}/${file}\n`);
         }
     })
     .option('addr', {
@@ -34,10 +46,20 @@ require('yargs')
         alias: 'p',
         default: '8000'
     })
-    .option('https', {
+    .option('tls', {
         describe: 'Enable https',
-        default: false
+        type: 'boolean'
     })
+    .option('cert', {
+        describle: 'Path to tls cert file',
+        default: 'cert.pem'
+    })
+    .alias('c', 'cert')
+    .option('key', {
+        describle: 'Path to tls key file',
+        default: 'key.pem'
+    })
+    .alias('k', 'key')
     .help('h')
     .alias('h', 'help')
     .argv;
